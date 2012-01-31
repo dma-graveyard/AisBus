14fd07a4f2f834ce6067522ce183d3ab44afa085
Ole Borup <obo@frv.dk>
2012-01-31 21:04:34 +0100

    Implemented service scripts

---
+++ bin/aisbus-service.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+SCRIPTPATH=`dirname $0`
+cd $SCRIPTPATH
+
+stop () {
+	# Find pid
+	PID=`./getpid.pl aisbus.AisBus`
+	if [ -z $PID ]; then
+		echo "AisBus not running"
+		exit 1
+	fi
+	echo "Stopping AisBus"
+	kill $PID
+    exit 0
+}
+
+
+case "$1" in
+start)
+	PID=`./getpid.pl aisbus.AisBus`
+	if [ ! -z $PID ]; then
+		echo "AisBus already running"
+		exit 1
+	fi
+    echo "Starting AisBus"
+    ./aisbus.sh > /dev/null 2>&1 &
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+*)
+    echo "Usage: $0 (start|stop|restart|help)"
+esac
+
+
+
+
